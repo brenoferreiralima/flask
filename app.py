41c5ef1e726f305940a1378eee66142ee6d97233
Breno Lima <brenoferreiralima@gmail.com>
2022-09-27 20:05:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-app.run()
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/contact")
+def contatos():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
